b0918c6943c27f15016eadb00eb6a63e8de3d2be
Feng Yu <F3n67u@outlook.com>
2015-08-27 19:47:43 +0800

    Moved 'forture cookie' functionality into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -10,20 +11,13 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
 	res.render('home');
 });
 
-var fortunes = [
-	'Conquer your fears or they will conquer you.',
-	'Rivers need springs.',
-	'Do not fear what you don\'t know.',
-	'You will have a pleasant surprise.',
-	'Whenever possible, keep it simple.'
-];
 app.get('/about', function (req, res) {
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', {fortune: randomFortune});
+	res.render('about', {fortune: fortune.getFortune()});
 });
 
 // custom 404 page
@@ -43,3 +37,4 @@ app.listen(app.get('port'), function () {
 	console.log('Express started on http://localhost:' +
 		app.get('port') + '; press Ctrl-C to terminate');
 });
+
