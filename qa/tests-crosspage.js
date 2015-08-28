1b20047dd29651c8e2d7b1fd13831d1cc4b95765
Feng Yu <F3n67u@outlook.com>
2015-08-29 05:05:39 +0800

    automating qa with grunt

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -29,7 +29,7 @@ suite('Cross-Page Tests', function () {
     });
   });
       
-  test('visiting the "request group rate" page directly shoule result ' +
+  test('visiting the "request group rate" page directly should result ' +
       'in an empty referrer field', function (done) {
     browser.visit('http://localhost:3000/tours/request-group-rate', function () {
       assert(browser.field('referrer').value === '');
