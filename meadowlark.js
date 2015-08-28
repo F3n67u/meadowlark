9c950b4e8da660f861842d3a8039a874003bbcb5
Feng Yu <F3n67u@outlook.com>
2015-08-29 00:02:57 +0800

    add cross page testing

--- meadowlark.js
+++ meadowlark.js
@@ -7,43 +7,51 @@ var app = express();
 // set up hanlebars view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
+// set port
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
-	res.locals.showTests = app.get('env') !== 'production' &&
-		req.query.test === '1';
-	next();
+  res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+  next();
 });
 
 app.get('/', function (req, res) {
-	res.render('home');
+  res.render('home');
 });
 
 app.get('/about', function (req, res) {
-	res.render('about', {
-		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js'
-	});
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+  res.render('tours/request-group-rate');
 });
 
 // custom 404 page
 app.use(function (req, res) {
-	res.status(404);
-	res.render('404');
+  res.status(404);
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function (err, req, res, next) {
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
-	console.log('Express started on http://localhost:' +
-		app.get('port') + '; press Ctrl-C to terminate');
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate');
 });
 
