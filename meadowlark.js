9a1b75993c48c817c494d4422a9232b6f7e28d93
Feng Yu <F3n67u@outlook.com>
2015-08-29 22:37:03 +0800

    add weather widget

--- meadowlark.js
+++ meadowlark.js
@@ -2,19 +2,59 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var fortune = require('./lib/fortune.js');
 
+// express initiation
 var app = express();
 
 // set up hanlebars view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-// set port
+
+// configure port
 app.set('port', process.env.PORT || 3000);
 
+// configure static resorces path
 app.use(express.static(__dirname + '/public'));
 
+// set 'showTests' context property if the querystring contains test=1
 app.use(function (req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' &&
     req.query.test === '1';
+  next();
+});
+
+// mocked weather data
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Portland',
+          forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+          iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+          weather: 'Overcast',
+          temp: '54.1 F (12.3 C)'
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)'
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)'
+      }
+    ]
+  };
+}
+
+// middleware to add weather data to context
+app.use(function (req, res, next) {
+  if (!res.locals.partialsData) res.locals.partialsData = {};
+  res.locals.partialsData.weather = getWeatherData();
   next();
 });
 
@@ -54,6 +94,7 @@ app.use(function (err, req, res, next) {
   res.render('500');
 });
 
+// server configuration
 app.listen(app.get('port'), function () {
   console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate');
